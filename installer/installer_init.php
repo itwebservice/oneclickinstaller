8262e9b98c3a0b598fc43c99ee735860a1194ead
SonamItweb <sonam.itweb@gmail.com>
2023-01-25 11:13:36 +0530

    ready data provide changes done

--- installer/installer_init.php
+++ installer/installer_init.php
@@ -25,7 +25,7 @@ $source_db = "v8";
 $source = "../../v7-version-upgrade";
 $destination = '../Projects/'.$product_name;
 
-$table_exclude = array('state_and_cities', 'user_assigned_roles', 'roles', 'role_master', 'bus_master', 'tour_budget_type', 'bank_name_master', 'bank_list_master', 'transport_agency_bus_master', 'city_master', 'currency_name_master', 'vendor_type_master', 'estimate_type_master', 'airport_list_master', 'references_master', 'country_state_list', 'country_list_master','gallary_master','destination_master','airline_master','airport_master','visa_crm_master','visa_type_master','sac_master','state_master','generic_count_master','office_expense_type','branch_assign','ledger_master','group_master','head_master','subgroup_master','cms_master','cms_master_entries','fixed_asset_master','meal_plan_master','modulewise_video_master','meal_plan_master','room_category_master','hotel_type_master','b2b_settings','b2b_settings_second','vehicle_type_master','tax_conditions','other_charges_master', 'ticket_master_airfile', 'ticket_entries_airfile', 'ticket_trip_entries_airfile','video_itinerary_master','app_settings','b2b_transfer_master','itinerary_master','tcs_master');
+$table_exclude = array('state_and_cities', 'user_assigned_roles', 'roles', 'role_master', 'bus_master', 'tour_budget_type', 'bank_name_master', 'bank_list_master', 'transport_agency_bus_master', 'city_master', 'currency_name_master', 'vendor_type_master', 'estimate_type_master', 'airport_list_master', 'references_master', 'country_state_list', 'country_list_master','gallary_master','destination_master','airline_master','airport_master','visa_crm_master','visa_type_master','sac_master','state_master','generic_count_master','office_expense_type','branch_assign','ledger_master','group_master','head_master','subgroup_master','cms_master','cms_master_entries','fixed_asset_master','meal_plan_master','modulewise_video_master','meal_plan_master','room_category_master','hotel_type_master','b2b_settings','b2b_settings_second','vehicle_type_master','tax_conditions','other_charges_master', 'ticket_master_airfile', 'ticket_entries_airfile', 'ticket_trip_entries_airfile','video_itinerary_master','app_settings','b2b_transfer_master','itinerary_master','tcs_master','hotel_master','hotel_vendor_images_entries','custom_package_master','custom_package_program','custom_package_hotels','custom_package_transport','custom_package_images');
 
 if (!file_exists($destination)){
     mkdir($destination, 0777, true);
@@ -176,9 +176,9 @@ if($empty_setup=="Yes"){
         if( !in_array($table_name, $table_exclude) ){
 
             if($conn->query("SHOW TABLES LIKE '".$table_name."'")->num_rows==1) {
-                $query = $conn->query("truncate $table_name");     
-                if(!$query){
-                    echo $query->error;
+                $query1 = $conn->query("truncate $table_name");     
+                if(!$query1){
+                    echo $query1->error;
                 }
             }            
         }   
@@ -189,13 +189,31 @@ if($empty_setup=="Yes"){
     $query = $conn->query("delete from role_master where role_id not in ('1', '2', '3','4','5','6','7')");
     $query = $conn->query("delete from office_expense_type where expense_type_id >= '21'");
 	$query = $conn->query("delete from roles where id!='1'");
-    $query = $conn->query("delete from ledger_master where ledger_id >= '233'");
+    // $query = $conn->query("delete from ledger_master where ledger_id >= '233'");
     $query = $conn->query("delete from group_master where group_id >= '22'");
     $query = $conn->query("delete from head_master where head_id >= '14'");
     $query = $conn->query("delete from subgroup_master where subgroup_id >= '114'");
     $query = $conn->query("delete from gallary_master where entry_id >= '1086'");
     $query = $conn->query("delete from sac_master where sac_id >= '13'");
     $query = $conn->query("delete from visa_type_master where visa_type_id >= '12'");
+    
+    // //////////////Ready data for hotel,packages START//////////////////////
+    // HOTEL(// 1.hotel_master,2.hotel_vendor_images_entries)
+    // PACKAGE TOUR
+    // 1.custom_package_master, 2.custom_package_program, 3.custom_package_hotels, 4.custom_package_transport, 5.custom_package_images, 6.ledger_master
+    $query = $conn->query("delete from hotel_master where hotel_id>='131'");
+    $query = $conn->query("delete from hotel_vendor_images_entries where hotel_id>='131'");
+    $query = $conn->query("delete from custom_package_master where package_id>='29'");
+
+    $query = $conn->query("delete from custom_package_program where package_id>='29'");
+    $query = $conn->query("delete from custom_package_hotels where package_id>='29'");
+    $query = $conn->query("delete from custom_package_transport where package_id>='29'");
+    $query = $conn->query("delete from custom_package_images where package_id>='29'");
+
+    $query = $conn->query("delete from ledger_master where ledger_id >= '363'");
+    // /////////////////////////END///////////////////////////////////////////
+    $query = $conn->query("UPDATE `ledger_master` SET `balance`=0 WHERE 1");
+
     if($b2c == 'true'){
         $generic_query = $conn->query("update app_settings set b2c_flag = '1' where setting_id='1'");
     }else{
